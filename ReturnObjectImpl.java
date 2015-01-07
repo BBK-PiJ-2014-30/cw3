ddd871c6efecd284160fa786d7264866f02347d3
BBK-PiJ-2014-30 <devonmullings@yahoo.co.uk>
2015-01-07 04:12:24 +0000

    ReturnObjectImpl

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -6,6 +6,8 @@ public class ReturnObjectImpl implements ReturnObject {
     private Object object;
     private String errorMessage;
 
+
+
     public ReturnObjectImpl(Object returnObject, String errorMessage) {
         this.object = returnObject;
         this.errorMessage = errorMessage;
@@ -14,18 +16,20 @@ public class ReturnObjectImpl implements ReturnObject {
     @Override
     public boolean hasError() {
         if (getReturnValue().equals(this.object))return false;
-        else return false;
+        else return true;
     }
 
     @Override
     public ErrorMessage getError() {
+        boolean value = hasError();
 
-        if (!this.equals(null)) return ErrorMessage.NO_ERROR ;
+        if ( value == true )return ErrorMessage.NO_ERROR ;
         else return null;
     }
 
     @Override
     public Object getReturnValue() {
+        boolean value =hasError();
         if (!this.equals(null)) return this.object ;
         else return null;
     }
