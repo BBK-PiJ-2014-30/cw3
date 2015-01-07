dc49a5720c30dfceba2da866b168e664cb3cd547
BBK-PiJ-2014-30 <devonmullings@yahoo.co.uk>
2015-01-07 01:49:13 +0000

    cw3

---
+++ ReturnObjectImpl.java
@@ -0,0 +1,25 @@
+/**
+ * Created by devon on 07/01/2015.
+ */
+public class ReturnObjectImpl {
+
+    public interface  ReturnObject {}
+
+
+
+    public boolean hasError {}
+
+
+
+    public ErrorMessage getError {}
+
+
+
+    public Object getReturnValue {}
+
+
+
+
+
+}
+
