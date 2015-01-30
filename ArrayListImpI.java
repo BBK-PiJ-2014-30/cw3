b055f407ea22d277558d41c819b945af94189678
BBK-PiJ-2014-30 <devonmullings@yahoo.co.uk>
2015-01-30 21:09:14 +0000

    update

--- ArrayListImpI.java
+++ ArrayListImpI.java
@@ -5,11 +5,11 @@ public class ArrayListImpI implements List {
 
 
     private static int limit;
-    int length;
+    static int s = 0;
 
-    private Object list [];
+    private Object list[];
 
-    public ArrayListImpI(){
+    public ArrayListImpI() {
         this.limit = 10;
 
         this.list = new Object[limit];
@@ -19,8 +19,8 @@ public class ArrayListImpI implements List {
 
     public ArrayListImpI(Object[] list) {
 
-        this.list = new Object[ position];
-        System.out.println(position);
+        this.list = new Object[limit];
+        s++;
 
 
     }
@@ -29,13 +29,14 @@ public class ArrayListImpI implements List {
     @Override
     public boolean isEmpty() {
         int counter;
-        int count=0;
+        int count = 0;
 
 
-        for (counter =0; counter < this.list.length-1; counter++) counter++;
+        for (counter = 0; counter < this.list.length - 1; counter++) counter++;
         {
-            if (!this.list[counter].equals(null)){
-                count++;}
+            if (!this.list[counter].equals(null)) {
+                count++;
+            }
             if (count >= 1) {
                 return false;
             }
@@ -47,9 +48,9 @@ public class ArrayListImpI implements List {
     @Override
     public int size() {
 
-        int counter =0;
+        int counter = 0;
 
-        for (int count =0; count < this.list.length-1; count++)
+        for (int count = 0; count < this.list.length - 1; count++)
             if (!this.list[count].equals(null)) {
                 counter++;
             }
@@ -59,48 +60,38 @@ public class ArrayListImpI implements List {
     @Override
     public ReturnObject get(int index) {
 
-        if( this.error(index).equals(ErrorMessage.NO_ERROR)){
-            return new ReturnObjectImpl (this.list[index],error(index));
-            
+        if (this.error(index).equals(ErrorMessage.NO_ERROR)) {
+            return new ReturnObjectImpl(this.list[index], error(index));
+
         }
 
-        return new ReturnObjectImpl( error(index));
+        return new ReturnObjectImpl(error(index));
     }
 
     @Override
     public ReturnObject remove(int index) {
-        if ( error(index).equals(ErrorMessage.NO_ERROR)){
+        if (error(index).equals(ErrorMessage.NO_ERROR)) {
             Object o = new Object();
-            this.list [index]= o;
+            this.list[index] = o;
             delete(index);
 
-            return new ReturnObjectImpl (o);
+            return new ReturnObjectImpl(o);
 
 
-            }
-            return new ReturnObjectImpl(error(index));
+        }
+        return new ReturnObjectImpl(error(index));
 
 
     }
 
 
-
     @Override
     public ReturnObject add(int index, Object item) {
-        if ( !item.equals(null) && error(index).equals(ErrorMessage.NO_ERROR)) {
+        if (!item.equals(null) && error(index).equals(ErrorMessage.NO_ERROR)) {
 
-            for (int count = 0; count < this.list.length - 1; count++) {
-                if (this.list[count].equals(null)) {
-                    this.list[count] = this.list[index];
-                    this.list[index] = item;
-                    return new ReturnObjectImpl(error(index));
-
-                }
-            }
-
-
-
-
+            addObject(index);
+            this.list[index] = item;
+            return new ReturnObjectImpl(this.list[index], error(index));
         }
 
 
@@ -109,72 +100,88 @@ public class ArrayListImpI implements List {
 
     @Override
     public ReturnObject add(Object item) {
-        if ( error(item).equals(null)){
+        int index = size() + 1 / 2;
+
+        if (error(item).equals(null)) {
             return new ReturnObjectImpl(error(item));
         }
 
 
-        if ( !item.equals(null)) {
-            this.list[limit - 1] = item;
+        if (!item.equals(null)) {
+            addObject(index);
+            this.list[index] = item;
         }
-        Object o = this.list[limit-1];
-        this.list[limit-1]= null;
-        return new ReturnObjectImpl(o);
+
+
+        return new ReturnObjectImpl(this.list[index]);
     }
-    public ErrorMessage error ( int index) {
+
+    public ErrorMessage error(int index) {
 
         ErrorMessage message = this.error(index);
 
 
-        if (index >= 0 && index <= this.limit - 1){
+        if (index >= 0 && index <= this.limit - 1) {
 
-            return ErrorMessage.NO_ERROR;}
-        else if ( index <0 || index >limit-1) {return ErrorMessage.INDEX_OUT_OF_BOUNDS;}
+            return ErrorMessage.NO_ERROR;
+        } else if (index < 0 || index > limit - 1) {
+            return ErrorMessage.INDEX_OUT_OF_BOUNDS;
+        }
         return null;
 
 
-
-
-
-
-
-
     }
-    public ErrorMessage error ( Object item){
-        if ( item.equals(null)){
-            return ErrorMessage.INVALID_ARGUMENT;}
+
+    public ErrorMessage error(Object item) {
+        if (item.equals(null)) {
+            return ErrorMessage.INVALID_ARGUMENT;
+        }
         return null;
     }
-    public void delete (int index ){
+
+    public void delete(int index) {
         int index1 = index;
-        if ( size()-1 == index){
-            this.list[index]= this.list[index+1];
+        if (size() - 1 == index) {
+            this.list[index] = this.list[index + 1];
 
         }
-        if (size()-1 > index) {
-            while (index != size() - 1) {
+        if (size() - 1 > index) {
+            while (index != size() - 1)
                 this.list[index] = this.list[index1++];
-                index++;
-            }
+
+            index++;
         }
-        if ( size()+1 > this.list.length) {
+    }
+
+    public void addObject(int index) {
+
+
+        int size1 = size() + 1;
+        int size2 = size();
+
+        if (size() + 1 > this.list.length) {
             int size = limit - 1 + 10;
             Object[] o = new Object[size];
             this.limit = size;
             o = this.list;
             this.list = o;
 
-            while (index != size() - 1) {
-                this.list[index1] = this.list[index++];
-                index1++;
+            while (index != size1) {
+                this.list[size1] = this.list[size2];
+                size1--;
+                size2--;
             }
-
         }
-
-
-
-            }
+    }
 }
+
+
+
+
+
+
+
+
 
 
 
